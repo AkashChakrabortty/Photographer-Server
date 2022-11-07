ce323e76b61242dcb4a979dd5a679d8258ae96cc
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-11-08 00:55:41 +0600

    get 3 items

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,9 +8,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('api found')
-})
+// mongo
+const uri = "mongodb+srv://dbuser1:esFcOiBVKyVU1lMR@cluster0.kusbv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+       const serviceCollection = client.db('database').collection('service');
+       
+        //find 3 element from database
+        app.get('/', async(req,res)=>{
+            const query ={};
+            const cursor = serviceCollection.find(query).limit(3);
+            const result = await cursor.toArray();
+            console.log(result)
+            res.send(result) 
+        })
+    }
+    catch{
+        (err => console.log(err))
+    }
+}
+run().catch(err => console.log(err))
+
+
 app.listen(port, ()=>{
     console.log('server running')
 })
