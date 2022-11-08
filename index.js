b196599fd9022890887ff35435726d559e06d52d
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-11-08 20:16:27 +0600

    find specific id info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,13 @@ async function run(){
             const result = await cursor.toArray();
             console.log(result)
             res.send(result) 
+        })
+        // find specific id info
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
         })
     }
     catch{
